b10187f50ef38c9f1de799e4548125379b3bf65b
arturohoyosdargent <arturohoyosdargent@gmail.com>
2026-09-10 13:52:28 -0500

    Refresh Cloud core cache version

--- firebase-cloud.js
+++ firebase-cloud.js
@@ -28,7 +28,7 @@
     document.head.appendChild(main);
     const core=document.createElement('script');
     core.type='module';
-    core.src='./firebase-cloud-core.js?v=24';
+    core.src='./firebase-cloud-core.js?v=25';
     document.head.appendChild(core);
     console.log('Préstamo Ya: núcleo principal cargado correctamente');
   }catch(e){
